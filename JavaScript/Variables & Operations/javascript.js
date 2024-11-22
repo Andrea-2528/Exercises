39e93917c97b7e8d2b9e79d956ccf74363328d8e
Andrea-2528 <188598696+Andrea-2528@users.noreply.github.com>
2024-11-22 12:27:02 +0100

    Add first JavaScript exercise

---
+++ JavaScript/Variables & Operations/javascript.js
@@ -0,0 +1,38 @@
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+
+
+
+
+let age = 11;
+console.log(age); // outputs 11 to the console
+
+age = 54;
+ 
+console.log(age); // what will be output now?
+
+
+// Exercises
+
+// 1
+console.log(23 + 97);
+
+//2
+console.log(4+8+15+16+23+42);
+
+//3
+console.log((4 + 6 + 9) / 77);
+
+//4
+let a=10;
+console.log(a);
+
+//5
+const max=57;
+let actual=max-13;
+let percentage=actual/max;
+console.log(percentage);
